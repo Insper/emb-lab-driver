1c01a223ec4b3d09f0f88c0a71b7d25147e611ef
Rafael Corsi <corsiferrao@gmail.com>
2023-08-22 21:10:36 -0300

    add solution lab 1

--- src/main.c
+++ src/main.c
@@ -1,18 +1,3 @@
-/**
- * 5 semestre - Eng. da Computação - Insper
- * Rafael Corsi - rafael.corsi@insper.edu.br
- *
- * Projeto 0 para a placa SAME70-XPLD
- *
- * Objetivo :
- *  - Introduzir ASF e HAL
- *  - Configuracao de clock
- *  - Configuracao pino In/Out
- *
- * Material :
- *  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
- */
-
 /************************************************************************/
 /* includes                                                             */
 /************************************************************************/
@@ -23,10 +8,45 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define LED_PIO PIOC       // periferico que controla o LED
+#define LED_PIO_ID ID_PIOC // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX 8      // ID do LED no PIO
+#define LED_PIO_IDX_MASK (1 << LED_PIO_IDX) // Mascara para CONTROLARMOS o LED
+
+#define BUT_PIO PIOA
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1 << BUT_PIO_IDX)
+
+#define LED1_PIO PIOA
+#define LED1_PIO_ID ID_PIOA
+#define LED1_PIO_IDX 0
+#define LED1_PIO_IDX_MASK (1 << LED1_PIO_IDX)
+
+#define BUT1_PIO PIOD
+#define BUT1_PIO_ID ID_PIOD
+#define BUT1_PIO_IDX 28
+#define BUT1_PIO_IDX_MASK (1 << BUT1_PIO_IDX)
+
+#define LED2_PIO PIOC
+#define LED2_PIO_ID ID_PIOC
+#define LED2_PIO_IDX 30
+#define LED2_PIO_IDX_MASK (1 << LED2_PIO_IDX)
+
+#define BUT2_PIO PIOC
+#define BUT2_PIO_ID ID_PIOC
+#define BUT2_PIO_IDX 31
+#define BUT2_PIO_IDX_MASK (1 << BUT2_PIO_IDX)
+
+#define LED3_PIO PIOB
+#define LED3_PIO_ID ID_PIOB
+#define LED3_PIO_IDX 2
+#define LED3_PIO_IDX_MASK (1 << LED3_PIO_IDX)
+
+#define BUT3_PIO PIOA
+#define BUT3_PIO_ID ID_PIOA
+#define BUT3_PIO_IDX 19
+#define BUT3_PIO_IDX_MASK (1 << BUT3_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -51,9 +71,35 @@ void init(void);
 /************************************************************************/
 
 // Função de inicialização do uC
-void init(void)
-{
+void init(void) {
+  // Initialize the board clock
+  sysclk_init();
 
+  // Disativa WatchDog Timer
+  WDT->WDT_MR = WDT_MR_WDDIS;
+
+  pmc_enable_periph_clk(LED_PIO_ID);
+  pmc_enable_periph_clk(LED1_PIO_ID);
+  pmc_enable_periph_clk(LED2_PIO_ID);
+  pmc_enable_periph_clk(LED3_PIO_ID);
+  pmc_enable_periph_clk(BUT1_PIO_ID);
+  pmc_enable_periph_clk(BUT2_PIO_ID);
+  pmc_enable_periph_clk(BUT3_PIO_ID);
+
+  // Inicializa PC8 como saída
+  pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+
+  pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+  pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+  pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+
+  pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+  pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+  pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+
+  pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+  pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+  pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
 }
 
 /************************************************************************/
@@ -61,15 +107,41 @@ void init(void)
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
-int main(void)
-{
+int main(void) {
   init();
 
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
-
+  while (1) {
+    if (!pio_get(BUT1_PIO, PIO_INPUT,
+                 BUT1_PIO_IDX_MASK)) { // Caso aperte Botao 1
+      for (int i = 0; i < 5; i++) {
+        pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+        delay_ms(200);
+        pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+        delay_ms(200);
+      }
+      pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);
+    }
+    if (!pio_get(BUT2_PIO, PIO_INPUT,
+                 BUT2_PIO_IDX_MASK)) { // Caso aperte Botao 2
+      for (int i = 0; i < 5; i++) {
+        pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+        delay_ms(200);
+        pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+        delay_ms(200);
+      }
+      pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
+    }
+    if (!pio_get(BUT3_PIO, PIO_INPUT,
+                 BUT3_PIO_IDX_MASK)) { // Caso aperte Botao 3
+      for (int i = 0; i < 5; i++) {
+        pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+        delay_ms(200);
+        pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+        delay_ms(200);
+      }
+      pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
+    }
   }
+  // nunca devemos chegar aqui!
   return 0;
 }
